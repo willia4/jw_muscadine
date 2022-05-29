d4498d8a7e4fb2e8998c49bae98115f52fe9ce8b
James Williams <james@jameswilliams.me>
2022-05-29 14:37:17 -0400

    Remove obsolete C# Mongo wrapper

--- src/ElectricLemur.Muscadine.MongoDriver/DocumentNotFoundException.cs
+++
@@ -1,6 +0,0 @@
-namespace ElectricLemur.Muscadine.MongoDriver;
-
-public class DocumentNotFoundException : Exception
-{
-    public DocumentNotFoundException(string id) : base($"Could not find document with id {id}") { }
-}
