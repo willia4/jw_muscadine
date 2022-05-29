eb77df673fde8cfd4f20776fd7f9601a539ee8ea
James Williams <james@jameswilliams.me>
2022-05-29 10:39:00 -0400

    Only store json/bson in Mongo
    Instead of trying to have the Mongo layer worry about type conversions and such, let the upper layer handle that. The Mongo layer will just take json and return json; it will, of course, natively store bson.

--- src/ElectricLemur.Muscadine.Site/Database.fs
+++ src/ElectricLemur.Muscadine.Site/Database.fs
@@ -4,9 +4,14 @@ open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Configuration
 open Giraffe
 open System.Threading.Tasks
-
+open Newtonsoft.Json.Linq
 
 type ADocument = { _id: string; SomeData: string }
+
+type JsonString = JsonString of string
+module JsonString =
+    let value (JsonString input) = input
+    let from (input: string) = JsonString(input)
 
 module private Mongo =
     open MongoDB.Driver
@@ -35,46 +40,48 @@ module private Mongo =
     
     let openDatabase: HttpContext -> Task<MongoDatabase> = getSettings >> openDatabaseForSettings
 
-    let private convertFromBson<'a> (d: BsonDocument) =
-        let json = d.ToJson()
-        Newtonsoft.Json.JsonConvert.DeserializeObject<'a>(json)
-        
-    let private convertToBson d =
-        let json = Newtonsoft.Json.JsonConvert.SerializeObject(d)
-        BsonDocument.Parse(json)
-        
-    let getDocuments<'a> (db: MongoDatabase) = task {
+    let private bsonToJson (bson: BsonDocument) = bson.ToJson() |> JsonString.from
+    let private jsonToJObject (json: JsonString) = JObject.Parse(json |> JsonString.value)
+    let private bsonToJObject = bsonToJson >> jsonToJObject
+
+    let private jsonToBson json = BsonDocument.Parse(json |> JsonString.value)
+
+    let private jObjectToJson (o: JObject) = Newtonsoft.Json.JsonConvert.SerializeObject(o) |> JsonString.from
+    let private jObjectToBson = jObjectToJson >> jsonToBson
+
+
+    let getDocuments (db: MongoDatabase) = task {
         use! cursor = db.Collection
                         .Find(FilterDefinition<BsonDocument>.Empty)
                         .Sort("{_id: 1}")
                         .ToCursorAsync()
 
-        let acc = new System.Collections.Generic.List<'a>()
+        let acc = new System.Collections.Generic.List<JObject>()
         
         let! hasNext = cursor.MoveNextAsync()
         
         let mutable continueLooping = hasNext
         while continueLooping do
-            acc.AddRange(cursor.Current |> Seq.map convertFromBson<'a>)   
+            acc.AddRange(cursor.Current |> Seq.map bsonToJObject)   
             let! hasNext = cursor.MoveNextAsync()
             continueLooping <- hasNext
         
-        let r: 'a seq = acc
+        let r: JObject seq = acc
         return r
     }
     
-    let getDocument<'a> (db: MongoDatabase) (id: string) = task {
+    let getDocument (db: MongoDatabase) (id: string) = task {
         let mutable filter = new BsonDocument()
         filter.["_id"] <- id
         
         let! item = db.Collection.Find(filter).FirstOrDefaultAsync()
         return match item with
                 | null -> None
-                | _ -> item |> convertFromBson<'a> |> Some
+                | _ -> item |> bsonToJObject |> Some
     }
     
-    let insertDocument (db: MongoDatabase) (document: 'a) = task {
-        let mutable bson = convertToBson document
+    let insertDocument (db: MongoDatabase) document = task {
+        let mutable bson =  jObjectToBson document
         let id =
             if bson.Contains("_id") && not (bson.["_id"].IsBsonNull) then
                 bson.["_id"].AsString
@@ -85,26 +92,31 @@ module private Mongo =
         return id
     }
 
+let private toJObject o = JObject.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(o))
+
+let private JObjectToADocument (o: JObject) =
+    { _id = o.Value<string>("_id"); SomeData = o.Value<string>("SomeData")}
+
 let foo (ctx: HttpContext) = task {
     let! db = Mongo.openDatabase ctx
     let doc = { _id = null; SomeData = "foo bar baz" }
-    return! doc |> Mongo.insertDocument db
+    return! doc |> toJObject |> Mongo.insertDocument db
     
 }
 
 let bar (ctx: HttpContext) = task {
     let! db = Mongo.openDatabase ctx
-    let! docs = Mongo.getDocuments<ADocument>(db)
-    let s = System.String.Join(", ", docs |> Seq.map (fun d -> d.SomeData ))
+    let! docs = Mongo.getDocuments(db)
+    let s = System.String.Join(", ", docs |> Seq.map JObjectToADocument |> Seq.map (fun d -> d.SomeData ))
     return s
 }
 
 let baz ctx = task {
     let! db = Mongo.openDatabase ctx
-    let! docs = Mongo.getDocuments<ADocument> db
-    let first = docs |> Seq.head
-    let! doc = first._id |> Mongo.getDocument<ADocument> db
+    let! docs = Mongo.getDocuments db
+    let first = docs |> Seq.head |> JObjectToADocument
+    let! doc = first._id |> Mongo.getDocument db
     return match doc with
-            | Some x -> x.SomeData
+            | Some x -> (x |> JObjectToADocument).SomeData
             | None -> "Not Found"
 }
